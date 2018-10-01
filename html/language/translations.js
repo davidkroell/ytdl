ee8736f79403ad133c81e8687e2918a9de602b34
martinrabensteiner <rabenst99@gmail.com>
2018-10-01 21:05:29 +0200

    Made Translations

--- html/language/translations.js
+++ html/language/translations.js
@@ -2,99 +2,45 @@
 
 var mlCodes = [
     {
-        code: "bg",
-        name: "Bulgarian"
-    },
-    {
-        code: "zh",
-        name: "Traditional"
-    },
-    {
-        code: "zh",
-        name: "Simplified"
-    },
-    {
-        code: "cs",
-        name: "Czech"
-    },
-    {
-        code: "da",
-        name: "Danish"
-    },
-    {
-        code: "nl",
-        name: "Dutch"
-    },
-    {
         code: "en",
         name: "English"
     },
     {
-        code: "et",
-        name: "Estonian"
-    },
-    {
         code: "de",
         name: "German"
-    },
-    {
-        code: "el",
-        name: "Greek"
-    },
-    {
-        code: "hu",
-        name: "Hungarian"
-    },
-    {
-        code: "it",
-        name: "Italian"
-    },
-    {
-        code: "no",
-        name: "Norwegian"
-    },
-    {
-        code: "pl",
-        name: "Polish"
-    },
-    {
-        code: "pt",
-        name: "Portuguese"
-    },
-    {
-        code: "ro",
-        name: "Romanian"
-    },
-    {
-        code: "sk",
-        name: "Slovak"
-    },
-    {
-        code: "ru",
-        name: "Russian"
-    },
-    {
-        code: "es",
-        name: "Spanish"
-    },
-    {
-        code: "sv",
-        name: "Swedish"
-    },
+    }
 ];
 var MLstrings = [
     {
-        English: "Sausages",
-        Polish: "Kiełbaski",
-        German: "Würste",
-        Russian: "Колбасные изделия",
-        Traditional: "香腸"
+        English: "URL of the video*",
+        German: "Link des Videos*"
     },
     {
-        English: "Carrot",
-        Polish: "Marchewka",
-        German: "Karotte",
-        Russian: "Морковь",
-        Traditional: "胡蘿蔔"
+        English: "Filename*",
+        German: "Dateiname*"
+    },
+    {
+        English: "Artist",
+        German: "Interpret"
+    },
+    {
+        English: "Album",
+        German: "Album"
+    },
+    {
+        English: "Start time",
+        German: "Startzeit*"
+    },
+    {
+        English: "End time",
+        German: "Endzeit"
+    },
+    {
+        English: "Download",
+        German: "Download"
+    },
+    {
+        English: "Copy download-URL",
+        German: "Download-Link kopieren"
     }
 ];
