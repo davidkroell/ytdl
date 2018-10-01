8927aef82adf69249a94c5f504660817254d7e26
martinrabensteiner <rabenst99@gmail.com>
2018-10-01 20:57:22 +0200

    Create translations.js

---
+++ html/language/translations.js
@@ -0,0 +1,100 @@
+// Language Switcher by https://benfrain.com/creating-a-language-switcher-in-javascript/
+
+var mlCodes = [
+    {
+        code: "bg",
+        name: "Bulgarian"
+    },
+    {
+        code: "zh",
+        name: "Traditional"
+    },
+    {
+        code: "zh",
+        name: "Simplified"
+    },
+    {
+        code: "cs",
+        name: "Czech"
+    },
+    {
+        code: "da",
+        name: "Danish"
+    },
+    {
+        code: "nl",
+        name: "Dutch"
+    },
+    {
+        code: "en",
+        name: "English"
+    },
+    {
+        code: "et",
+        name: "Estonian"
+    },
+    {
+        code: "de",
+        name: "German"
+    },
+    {
+        code: "el",
+        name: "Greek"
+    },
+    {
+        code: "hu",
+        name: "Hungarian"
+    },
+    {
+        code: "it",
+        name: "Italian"
+    },
+    {
+        code: "no",
+        name: "Norwegian"
+    },
+    {
+        code: "pl",
+        name: "Polish"
+    },
+    {
+        code: "pt",
+        name: "Portuguese"
+    },
+    {
+        code: "ro",
+        name: "Romanian"
+    },
+    {
+        code: "sk",
+        name: "Slovak"
+    },
+    {
+        code: "ru",
+        name: "Russian"
+    },
+    {
+        code: "es",
+        name: "Spanish"
+    },
+    {
+        code: "sv",
+        name: "Swedish"
+    },
+];
+var MLstrings = [
+    {
+        English: "Sausages",
+        Polish: "Kiełbaski",
+        German: "Würste",
+        Russian: "Колбасные изделия",
+        Traditional: "香腸"
+    },
+    {
+        English: "Carrot",
+        Polish: "Marchewka",
+        German: "Karotte",
+        Russian: "Морковь",
+        Traditional: "胡蘿蔔"
+    }
+];
