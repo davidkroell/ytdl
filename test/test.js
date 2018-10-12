59599cfe84532aecd09e0d5ca2b597d3a218f657
Jeroen <jpa.beemsterboer@gmail.com>
2018-10-12 22:56:11 +0200

    Add first unit test to project

---
+++ test/test.js
@@ -0,0 +1,20 @@
+process.env.NODE_ENV = 'test';
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../../ytdl');
+/* eslint-disable no-unused-vars */
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+ describe('/GET homepage', () => {
+    it('it should GET the homepage', (done) => {
+        chai.request(server)
+            .get('/')
+            .end((err, res) => {
+                  res.should.have.status(200);
+              done();
+            });
+      });
+  });
