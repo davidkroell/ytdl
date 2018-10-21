15dae4e7fda0c705e0d4e73a7d01dbbb42496d2e
Akashdeep Ramnaney <akashdeepramnaney@gmail.com>
2018-10-21 19:24:05 +0530

    Updated the queries to Match SQL Style

--- statistical_analysis.sql
+++ statistical_analysis.sql
@@ -1,12 +1,30 @@
 #Statistical Analysis of Download Statistics
 #Top 10 Downloaded Videos in the Last 7 Days
-select video_id,title,album,artist from download_stats WHERE timestamp <= NOW() AND timestamp >= NOW() - INTERVAL 7 DAY group by video_id order by count(video_id) desc limit 10;
+SELECT video_id,title,album,artist 
+FROM download_stats 
+WHERE timestamp <= NOW() AND timestamp >= NOW() - INTERVAL 7 DAY 
+GROUP BY video_id 
+ORDER BY COUNT(video_id) DESC 
+LIMIT 10;
 
 #Most Popular Artists of the Month
-select artist from download_stats WHERE timestamp <= NOW() AND timestamp >= NOW() - INTERVAL 30 DAY group by artist order by count(artist) desc limit 5;
+SELECT artist 
+FROM download_stats 
+WHERE timestamp <= NOW() AND timestamp >= NOW() - INTERVAL 30 DAY 
+GROUP BY artist 
+ORDER BY COUNT(artist) DESC 
+LIMIT 5;
 
 #Top 3 Highest Downloaded Albums of All time
-select album from download_stats group by album order by count(album) desc limit 3;
+SELECT album 
+FROM download_stats 
+GROUP BY album 
+ORDER BY COUNT(album) DESC 
+LIMIT 3;
 
 #Highest Traffic(Countries) in the last 7 days
-select country from download_stats WHERE timestamp <= NOW() AND timestamp >= NOW() - INTERVAL 7 DAY group by country order by count(country) desc limit 5;
+SELECT country 
+FROM download_stats 
+WHERE timestamp <= NOW() AND timestamp >= NOW() - INTERVAL 7 DAY GROUP BY country 
+ORDER BY COUNT(country) DESC 
+LIMIT 5;
